30f0759cde1b71cfba1e92051d41fec7b571ee8d
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-23 18:22:53 -0400

    Logo and some styling

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,13 @@
-<div class="container">
-  <a href="#">Test1</a>
-  <a href="#">Test2</a>
-  <a href="#">Test3</a>
-  <a href="#">Test4</a>
+<script>
+  import logo from "../assets/logo.webp";
+</script>
+
+<div class="flex px-10 py-2">
+  <img src={logo} alt="CAPANDA logo" width="248px" height="72px">
+  <div class=" flex my-auto">
+    <a href="#">Test1</a>
+    <a href="#">Test2</a>
+    <a href="#">Test3</a>
+    <a href="#">Test4</a>
+  </div>
 </div>
