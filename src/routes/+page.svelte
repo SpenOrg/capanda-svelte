1ad211fcdf827c7949d8b33d41213a6fde929177
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-24 15:57:37 -0400

    Placeholder second panel for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,13 @@
   import Navbar from "$lib/components/Navbar.svelte";
   import panorama from "$lib/assets/panorama.webp";
   import logotext from "$lib/assets/logo-text.webp";
+
+
 </script>
 
 <Navbar/>
 
+<!-- First Panel -->
 <div class="hero min-h-screen" style="background-image: url({panorama});">
   <div class="hero-overlay row-auto bg-opacity-0"></div>
   <div class="hero-content text-center text-neutral-content">
@@ -16,6 +19,18 @@
       </h1>
       <p class="text-xl mb-5">Through hands-on Corporate leadership, CAPANDA will advance your Product, Program, Business-Process, and Software Asset Management needs by applying experience in sustainable digital architecture via service delivery principles & best practices</p>
       <button class="btn btn-secondary">Hire Us</button>
+    </div>
+  </div>
+</div>
+
+<!-- Second Panel -->
+<div class="hero min-h-screen bg-capanda-blue text-white">
+  <div class="hero-content flex-col lg:flex-row">
+    <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
