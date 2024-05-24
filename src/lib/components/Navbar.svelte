4809868ce196c23a8a0cdef64a3d02e08b2c1488
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-23 20:35:08 -0400

    Add DaisyUI and begin navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,12 +2,19 @@
   import logo from "../assets/logo.webp";
 </script>
 
-<div class="flex px-10 py-2">
-  <img src={logo} alt="CAPANDA logo" width="248px" height="72px">
-  <div class=" flex my-auto">
-    <a href="#">Test1</a>
-    <a href="#">Test2</a>
-    <a href="#">Test3</a>
-    <a href="#">Test4</a>
+<div class="navbar bg-base-100 2xl:px-56 xl:px-28 lg:px-10">
+  <div class="navbar-start">
+    <img src={logo} alt="CAPANDA logo" width="248px" height="72px">
+  </div>
+  <div class="navbar-center">
+    <ul class="menu menu-horizontal max-lg:hidden lg:text-xl">
+      <li><a>Value</a></li>
+      <li><a>Leadership</a></li>
+      <li><a>Dedication</a></li>
+      <li><a>Focus</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <a class="btn">Button</a>
   </div>
 </div>
