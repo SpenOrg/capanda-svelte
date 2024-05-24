d414d1c97703385cbd2cf46e8f326a1346f9c5a6
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-24 14:59:27 -0400

    Add content for first section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,14 @@
 </script>
 
 <Navbar/>
+
+<div class="text-center">
+    <h1>
+        <div>Build Sustainable Services with</div>
+        <div>CAPANDA</div>
+    </h1>
+    <p>Through hands-on Corporate leadership, CAPANDA will advance your Product,
+        Program, Business-Process, and Software Asset Management needs by applying experience in
+        sustainable digital architecture via service delivery principles & best practices</p>
+    <a class="btn font-bold">Hire Us</a>
+</div>
