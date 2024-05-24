0976c516742157eeb3d1484f53714a8d23733f6f
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-24 15:44:26 -0400

    Refine first panel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,21 @@
 <script>
-  import Navbar from "../lib/components/Navbar.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
+  import panorama from "$lib/assets/panorama.webp";
+  import logotext from "$lib/assets/logo-text.webp";
 </script>
 
 <Navbar/>
 
-<div class="text-center">
-    <h1>
-        <div>Build Sustainable Services with</div>
-        <div>CAPANDA</div>
-    </h1>
-    <p>Through hands-on Corporate leadership, CAPANDA will advance your Product,
-        Program, Business-Process, and Software Asset Management needs by applying experience in
-        sustainable digital architecture via service delivery principles & best practices</p>
-    <a class="btn font-bold">Hire Us</a>
+<div class="hero min-h-screen" style="background-image: url({panorama});">
+  <div class="hero-overlay row-auto bg-opacity-0"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div>
+      <h1>
+        <div class="text-7xl font-light">Build Sustainable Services with</div>
+        <img class="my-5 mx-auto max-h-14" src={logotext} alt="CAPANDA">
+      </h1>
+      <p class="text-xl mb-5">Through hands-on Corporate leadership, CAPANDA will advance your Product, Program, Business-Process, and Software Asset Management needs by applying experience in sustainable digital architecture via service delivery principles & best practices</p>
+      <button class="btn btn-secondary">Hire Us</button>
+    </div>
+  </div>
 </div>
