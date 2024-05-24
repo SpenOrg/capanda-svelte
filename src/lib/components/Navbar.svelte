b772d809f621ad1d6c8fb62b2063e5e613c19942
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-24 14:06:58 -0400

    Add custom colours to DaisyUI and customize the navbar a bit

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,7 +2,7 @@
   import logo from "../assets/logo.webp";
 </script>
 
-<div class="navbar bg-base-100 2xl:px-56 xl:px-28 lg:px-10">
+<div class="navbar bg-capanda-light-grey 2xl:px-56 xl:px-28 lg:px-10">
   <div class="navbar-start">
     <img src={logo} alt="CAPANDA logo" width="248px" height="72px">
   </div>
@@ -15,6 +15,6 @@
     </ul>
   </div>
   <div class="navbar-end">
-    <a class="btn">Button</a>
+    <a class="btn btn-accent">Button</a>
   </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const colors = {
+  'capanda-light-grey': '#f8f9fa',
+  'capanda-blue': '#0e2c57',
+  'capanda-grey': '#ededed',
+  'capanda-dark-grey': '#14171c',
+  'capanda-red': '#aa0000',
+};
+
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      colors: {
-        'capanda-light-grey': '#f8f9fa',
-        'capanda-blue': '#0e2c57',
-        'capanda-grey': '#ededed',
-        'capanda-dark-grey': '#14171c',
-        'capanda-red': '#aa0000',
-      },
+      colors: colors,
     },
   },
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        capanda: {
+          "accent": colors['capanda-red'],
+          "neutral": colors['capanda-blue'],
+          "secondary": colors['capanda-light-grey'],
+          "base-200": colors['capanda-grey'],
+          "base-300": colors['capanda-dark-grey'],
+        },
+      },
+    ],
+  },
 }
