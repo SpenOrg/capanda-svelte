c238d73711c8e92fc498b8526796b91fba181e11
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-25 16:55:09 -0400

    alt text

--- src/lib/components/Panel.svelte
+++ src/lib/components/Panel.svelte
@@ -11,7 +11,7 @@
 <div class="absolute min-h-screen w-screen {bgColorClass} {textColorClass} py-10" style="top:{top}px">
     <div class="relative mx-5 xl:m-auto max-w-6xl" style="top:100px">
         <div class="m-auto max-w-fit flex flex-col {imageRight ? 'lg:flex-row-reverse' : 'lg:flex-row'} items-center">
-            <img src={imageUrl} class="max-w-sm" />
+            <img src={imageUrl} class="max-w-sm" alt=""/>
             <div class="mt-5 lg:ml-10 lg:mt-0 text-center lg:text-left">
                 <h1 class="text-5xl font-bold">{header}</h1>
                 <div class="pt-6">
