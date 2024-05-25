ea601506d14c0f00131ddcc9d4b0feb184e72f91
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-25 16:25:19 -0400

    use components for panels

---
+++ src/lib/components/Panel.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let imageUrl;
+    export let header;
+    export let text;
+    export let top;
+</script>
+
+<div class="absolute min-h-screen w-screen bg-capanda-blue text-white py-10" style="top:{top}px">
+    <div class="relative mx-5 xl:m-auto max-w-6xl" style="top:100px">
+        <div class="m-auto max-w-fit flex flex-col lg:flex-row items-center">
+        <img src={imageUrl} class="max-w-sm rounded-lg shadow-2xl" />
+        <div class="mt-5 lg:ml-10 lg:mt-0 text-center lg:text-left">
+            <h1 class="text-5xl font-bold">{header}</h1>
+            <p class="py-6">{text}</p>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
+        </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import Navbar from "$lib/components/Navbar.svelte";
   import panorama from "$lib/assets/panorama.webp";
   import logotext from "$lib/assets/logo-text.webp";
+  import Panel from "$lib/Components/Panel.svelte";
   import {onMount} from "svelte";
 
   let viewportHeight = 0;
@@ -21,30 +22,25 @@
       window.removeEventListener('resize', updateviewportHeight);
     };
   });
-
-  $: console.log(viewportHeight);
 </script>
 
 <Navbar/>
 
 <!-- First Panel -->
-<div class="absolute min-h-screen w-screen text-center text-neutral-content py-10" style="background-image: url({panorama});">
-  <h1>
-    <div class="text-7xl font-light">Build Sustainable Services with</div>
-    <img class="my-5 mx-auto max-h-14" src={logotext} alt="CAPANDA">
-  </h1>
-  <p class="text-xl mb-5">Through hands-on Corporate leadership, CAPANDA will advance your Product, Program, Business-Process, and Software Asset Management needs by applying experience in sustainable digital architecture via service delivery principles & best practices</p>
-  <button class="btn btn-secondary">Hire Us</button>
-</div>
-
-<!-- Second Panel -->
-<div class="absolute min-h-screen w-screen bg-capanda-blue text-white py-10" style="top:{viewportHeight * 1}px">
-  <div class="m-auto max-w-fit flex flex-col lg:flex-row items-center">
-    <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
-    <div class="mt-5 lg:ml-10 lg:mt-0 text-center lg:text-left">
-      <h1 class="text-5xl font-bold">Box Office News!</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button class="btn btn-primary">Get Started</button>
-    </div>
+<div class="absolute min-h-screen w-screen text-center text-neutral-content py-10" style="background-image: url({panorama})">
+  <div class="relative mx-5 xl:m-auto max-w-6xl" style="top:100px">
+    <h1>
+      <div class="text-7xl font-light">Build Sustainable Services with</div>
+      <img class="my-5 mx-auto max-h-14" src={logotext} alt="CAPANDA">
+    </h1>
+    <p class="text-xl mb-5">Through hands-on Corporate leadership, CAPANDA will advance your Product, Program, Business-Process, and Software Asset Management needs by applying experience in sustainable digital architecture via service delivery principles & best practices</p>
+    <button class="btn btn-secondary">Hire Us</button>
   </div>
 </div>
+
+<Panel
+  imageUrl="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+  header="Box Office News!"
+  text="Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi."
+  top={viewportHeight}
+/>
