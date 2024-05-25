e606c7bbbd0cd0bb412250a4cd6e1f1f67c8658f
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-25 16:54:50 -0400

    Add images, refine styling and allow multiple paragraphs

--- src/lib/components/Panel.svelte
+++ src/lib/components/Panel.svelte
@@ -1,7 +1,7 @@
 <script>
     export let imageUrl;
     export let header;
-    export let text;
+    export let text = [];
     export let bgColorClass;
     export let textColorClass;
     export let top;
@@ -11,10 +11,14 @@
 <div class="absolute min-h-screen w-screen {bgColorClass} {textColorClass} py-10" style="top:{top}px">
     <div class="relative mx-5 xl:m-auto max-w-6xl" style="top:100px">
         <div class="m-auto max-w-fit flex flex-col {imageRight ? 'lg:flex-row-reverse' : 'lg:flex-row'} items-center">
-            <img src={imageUrl} class="max-w-sm rounded-lg shadow-2xl" />
+            <img src={imageUrl} class="max-w-sm" />
             <div class="mt-5 lg:ml-10 lg:mt-0 text-center lg:text-left">
                 <h1 class="text-5xl font-bold">{header}</h1>
-                <p class="py-6">{text}</p>
+                <div class="pt-6">
+                    {#each text as paragraph}
+                        <p class=" text-lg py-2">{paragraph}</p>
+                    {/each}
+                </div>
             </div>
         </div>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script>
   import Navbar from "$lib/components/Navbar.svelte";
   import panorama from "$lib/assets/panorama.webp";
+  import valueImg from "$lib/assets/value.webp";
+  import leadershipImg from "$lib/assets/leadership.webp";
+  import focusImg from "$lib/assets/focus.webp";
+  import dedicationImg from "$lib/assets/dedication.webp";
   import logotext from "$lib/assets/logo-text.webp";
   import Panel from "$lib/Components/Panel.svelte";
   import {onMount} from "svelte";
@@ -39,9 +43,15 @@
 </div>
 
 <Panel
-  imageUrl="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-  header="Box Office News!"
-  text="Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi."
+  imageUrl={valueImg}
+  header="Our Value"
+  text={[
+    "Sustainability is our Mantra",
+    "Whether through the delivery of proven digital architectures, or via Organizational Change Management practices (including everything in-between), sustainable practices will ensure long-term value for your organization",
+    "Through the delivery of proven forward-looking practices, your organization will recognize the value by eliminating the typical cycle of repeating itself",
+    "Don’t relive your old ways by seeking heroes to keep your head above water – True value comes from sustainable & proven architectural practices that bind themselves to your organization’s DNA",
+    "Digital transformation is more than just changing or upgrading, true value is the result of applying proven practices towards sustainable services that remain nimble to adapt to the ever-changing digital world"
+  ]}
   top={viewportHeight}
   bgColorClass="bg-capanda-blue"
   textColorClass="text-white"
