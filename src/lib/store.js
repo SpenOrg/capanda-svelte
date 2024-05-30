1a4024f65c37c0550fc04d27d15e7347d93f6e73
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-30 12:19:54 -0400

    Revert "Better way of getting the directory"
    This reverts commit 44aace6828ad64c823185771b22d3b111886f71b.

--- src/lib/store.js
+++ src/lib/store.js
@@ -3,3 +3,4 @@ import { writable } from 'svelte/store';
 export const panelInstanceCount = writable(-1); //Start at -1 so the first panel starts at 0
 export const panelVisibility = writable({});
 export const currentPanel = writable(0);
+export const basePath = "/";
