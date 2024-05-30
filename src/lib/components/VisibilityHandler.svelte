709ba1a8dc9f664cb1a63273ba2e1459575bc08b
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-05-30 12:24:44 -0400

    Revert "Better way of getting the directory"
    This reverts commit 44aace6828ad64c823185771b22d3b111886f71b.

--- src/lib/components/VisibilityHandler.svelte
+++ src/lib/components/VisibilityHandler.svelte
@@ -2,10 +2,9 @@
     import { panelVisibility, basePath } from '$lib/store.js';
     import { onMount, tick } from 'svelte';
     import { goto } from "$app/navigation";
-    import { base } from "$app/paths"
 
     let panel;
-    let panelOrder = [`${base}/value`, `${base}/leadership`, `${base}/dedication`, `${base}/focus`];
+    let panelOrder = [`${basePath}value`, `${basePath}leadership`, `${basePath}dedication`, `${basePath}focus`];
 
     let currentPanelIndex;
     // async function customGoto(url) {
