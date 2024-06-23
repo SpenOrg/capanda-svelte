b8d04094ae32d14951463c691d28ba5496940769
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-06-23 14:34:52 -0400

    Move navbar to layout.svelte

--- src/routes/value/+page.svelte
+++ src/routes/value/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-    import Navbar from "$lib/components/Navbar.svelte";
     import valueImg from "$lib/assets/value.webp";
     import Panel from "$lib/components/Panel.svelte";
     import PanelPlaceholder from "$lib/components/PanelPlaceholder.svelte"
@@ -8,8 +7,6 @@
 </script>
 
 <BackgroundColorHandler/>
-
-<Navbar/>
 
 <PanelPlaceholder/>
 
